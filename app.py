afbfdf8f057ba14627a3a73a56954e955f1dacc2
Masoud NAVIDI <navidi.m.91@gmail.com>
2024-06-23 18:25:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
+@app.route("/")
 @app.route("/index.html")
 def home():
     return render_template("index.html")
